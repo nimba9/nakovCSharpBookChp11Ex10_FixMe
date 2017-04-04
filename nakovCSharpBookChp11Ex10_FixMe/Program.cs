c07fb20ec07b27eeec694b7e3620af3fbe9d77f5
vancisin peter <vancisin.peter@gmail.com>
2017-04-04 13:59:45 +0200

    Add project files.

---
+++ nakovCSharpBookChp11Ex10_FixMe/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+/* 
+ * The following program contains an error (or more {the more the merrier :-)}) made 
+ * during the process of solving.
+ * It serves as a remainder and memory training.
+ */
+
+
+namespace nakovCSharpBookChp11Ex10_FixMe
+{
+    public class StringNumbersAdding
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter number separated by comma, point or space: ");
+
+            string userInp = Console.ReadLine();
+
+            SumNumbers(userInp);
+        }
+
+        public static void SumNumbers(string userInp)
+
+
+        {
+            string digits = userInp.Trim(new Char[] { ' ', '*', '.' }); // The new Char part was copied online
+
+            int sum = 0;
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                sum += digits.add();
+
+                if (digits != ' ')
+                {
+                    int num;
+                    digits.TryParse(digits, out num);
+
+                }
+
+            }
+
+        }
+    }
+}
+
